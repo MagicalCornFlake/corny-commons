0ebc8b52da5ea1d8f6e631facecabda2abd0b0b3
Konrad (PC) <kolego199@gmail.com>
2022-01-23 04:04:23 +0100

    swap convert function argument order

--- util/currency.py
+++ util/currency.py
@@ -24,7 +24,7 @@ def update_existing_cache() -> None:
         file_manager.write_cache("exchange_rates", get_new_data())
 
 
-def convert(to: str, value: float, digits: int = 2) -> float:
+def convert(value: float, to: str, digits: int = 2) -> float:
     """Converts the specified value from USD to the given currency."""
     update_existing_cache()
     data = file_manager.get_cache("exchange_rates", False, get_new_data)
@@ -42,5 +42,5 @@ if __name__ == "__main__":
     while True:
         raw = input("Type target currency and conversion amount...\n> ")
         to, value = raw.split(' ', maxsplit=1)
-        result = convert(to, float(value))
+        result = convert(float(value), to)
         print(f"${value} = {result} {to}")
