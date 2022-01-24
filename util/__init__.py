2610d784776f2cbe9c5face4ad95c8c47a241f63
Konrad (PC) <kolego199@gmail.com>
2022-01-24 18:07:02 +0100

    add format time functions

--- util/__init__.py
+++ util/__init__.py
@@ -13,3 +13,22 @@ def format_exception_info(exception: Exception):
     info = type(exception), exception, exception.__traceback__
     fmt_info = traceback.format_exception(*info)
     return ''.join(fmt_info)
+
+
+def format_time(val: int, word: str):
+    """(12, 'second') -> '12 seconds '."""
+    return f"{val} {word}{'s' * int(val != 1)} " * bool(val)
+
+
+def format_seconds(seconds: int):
+    """Formats the number of seconds as hours, minutes, seconds.
+
+    E.g. 3777 -> '1 hour 2 minutes 57 seconds'
+    """
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+
+    hours = format_time(hours, "hour")
+    minutes = format_time(minutes,"minute")
+    seconds = format_time(seconds, "second")
+    return hours + minutes + seconds
--- util/currency.py
+++ util/currency.py
@@ -11,6 +11,7 @@ SOURCE_URL = "https://open.er-api.com/v6/latest/USD"
 
 
 def get_new_data() -> dict:
+    """Makes a web request to the API and returns the new JSON data."""
     return web.make_request(SOURCE_URL).json()
 
 
@@ -18,6 +19,7 @@ def update_existing_cache() -> None:
     """Overwrites existing cache that has been updated on the API with the new data."""
     existing_data = file_manager.read_cache("exchange_rates")
     if not existing_data:
+        # The cache does not exist
         return
     next_update_time: int = existing_data.get("time_next_update_unix", 0)
     if time.time() > next_update_time:
@@ -26,16 +28,14 @@ def update_existing_cache() -> None:
 
 def convert(value: float, to: str, frm: str = "USD", digits: int = 2) -> float:
     """Converts the specified value from USD to the given currency.
-    
+
     Arguments:
         value -- the monetary value to convert.
         to -- the ISO code for the currency to convert to.
         frm -- the ISO code for the currency to convert from (defaults to USD).
         digits -- the number of digits the result should be rounded to (defaults to 2).
         If this is negative, the result won't be rounded."""
-    update_existing_cache()
-    data = file_manager.get_cache("exchange_rates", False, get_new_data)
-    rates: dict[str, int] = data[0].get("rates")
+    rates: dict[str, int] = get_exchange_rates()
     exchange_rate_to = rates.get(to)
     exchange_rate_from = rates.get(frm)
     if not exchange_rate_to or not exchange_rate_from:
@@ -46,12 +46,24 @@ def convert(value: float, to: str, frm: str = "USD", digits: int = 2) -> float:
     return result
 
 
+def get_exchange_rates() -> dict:
+    """Returns the currently cached exchange rate data."""
+    update_existing_cache()
+    return file_manager.get_cache("exchange_rates", False, get_new_data)[0].get("rates")
+
+
+def get_currencies() -> list[str]:
+    """Returns a list of ISO currency formats that can be used in the `convert()` function."""
+    return list(get_exchange_rates().keys())
+
+
 if __name__ == "__main__":
     try:
         while True:
-            raw = input("Type origin currency, target currency and conversion amount...\n> ")
-            frm, to, value = raw.split(' ', maxsplit=2)
-            result = convert(float(value), to, frm, -1)
-            print(f"{frm} {value} = {to} {result}")
+            raw = input(
+                "Type origin currency, target currency and conversion amount...\n> ")
+            _frm, _to, _value = raw.split(' ', maxsplit=2)
+            _result = convert(float(_value), _to, _frm, -1)
+            print(f"{_frm} {_value} = {_to} {_result}")
     except KeyboardInterrupt:
         print("...\nGoodbye!")
