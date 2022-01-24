b9547656833f4130e12542d7e1d4e87029c73699
Konrad (PC) <kolego199@gmail.com>
2022-01-24 13:42:58 +0100

    implement currency conversions from non-USD

--- util/currency.py
+++ util/currency.py
@@ -24,23 +24,34 @@ def update_existing_cache() -> None:
         file_manager.write_cache("exchange_rates", get_new_data())
 
 
-def convert(value: float, to: str, digits: int = 2) -> float:
-    """Converts the specified value from USD to the given currency."""
+def convert(value: float, to: str, frm: str = "USD", digits: int = 2) -> float:
+    """Converts the specified value from USD to the given currency.
+    
+    Arguments:
+        value -- the monetary value to convert.
+        to -- the ISO code for the currency to convert to.
+        frm -- the ISO code for the currency to convert from (defaults to USD).
+        digits -- the number of digits the result should be rounded to (defaults to 2).
+        If this is negative, the result won't be rounded."""
     update_existing_cache()
     data = file_manager.get_cache("exchange_rates", False, get_new_data)
     rates: dict[str, int] = data[0].get("rates")
-    exchange_rate = rates.get(to)
-    if not exchange_rate:
+    exchange_rate_to = rates.get(to)
+    exchange_rate_from = rates.get(frm)
+    if not exchange_rate_to or not exchange_rate_from:
         raise ValueError(f"Unsupported currency: '{to}'")
-    result = value * exchange_rate
+    result = value * exchange_rate_to / exchange_rate_from
     if isinstance(digits, int) and digits >= 0:
         return round(result, digits)
     return result
 
 
 if __name__ == "__main__":
-    while True:
-        raw = input("Type target currency and conversion amount...\n> ")
-        to, value = raw.split(' ', maxsplit=1)
-        result = convert(float(value), to)
-        print(f"${value} = {result} {to}")
+    try:
+        while True:
+            raw = input("Type origin currency, target currency and conversion amount...\n> ")
+            frm, to, value = raw.split(' ', maxsplit=2)
+            result = convert(float(value), to, frm, -1)
+            print(f"{frm} {value} = {to} {result}")
+    except KeyboardInterrupt:
+        print("...\nGoodbye!")
